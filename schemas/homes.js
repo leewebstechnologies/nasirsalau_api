e60aed3de15b46a8e262c2cefca7b5023aa917ed
leewebstechnologies <aliuajibade317@gmail.com>
2024-01-07 22:35:14 +0100

    contact api fixed

---
+++ schemas/homes.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'homes',
+  title: 'Homes',
+  type: 'document',
+  fields: [
+    {
+      name: 'figure',
+      title: 'Figure',
+      type: 'number',
+    },
+    {
+      name: 'percentage',
+      title: 'Percentage',
+      type: 'number',
+    },
+    {
+      name: 'amount',
+      title: 'Amount',
+      type: 'number',
+    },
+  ],
+}
--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,7 @@
+import homes from './homes'
 import cases from './cases'
 import contact from './contact'
 import practice from './practice'
 import subscribe from './subscribe'
 import team from './team'
-export const schemaTypes = [practice, cases, team, contact, subscribe]
+export const schemaTypes = [homes, practice, cases, team, contact, subscribe]
